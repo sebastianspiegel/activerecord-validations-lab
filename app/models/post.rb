17944a14c8401075c68e9b8227397d3320c1b036
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-26 11:28:42 -0500

    commit tree 3c90421291500279cf1962e63e08ca9fa62321cf

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Nonfiction'] }
+    validate :clickbait
+
+    CLICKBAIT_TITLES = [/Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+
+
+    def clickbait
+        if CLICKBAIT_TITLES.none? { |name| name.match title}
+            errors.add(:title, "not clickbait")
+        end
+    end
+
 end
+
